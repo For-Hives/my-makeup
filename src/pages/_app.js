8e95fbcfa88902c6ae3a1a48b2fcfe7f3ce6fe26
breval <breval2000@live.fr>
2023-03-28 21:56:57 +0200

    Mise en place de pre-commit-hook pour prettier

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
-import '@/styles/globals.css'
-import '@/styles/icons.css'
-import Head from "next/head";
+import '@/styles/globals.css';
+import '@/styles/icons.css';
+import Head from 'next/head';
 
-export default function App({Component, pageProps}) {
-    return (<>
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        </Head>
-        <Component {...pageProps} />
-    </>)
+export default function App({ Component, pageProps }) {
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,48 @@
-import Head from 'next/head'
-import {Inter} from 'next/font/google'
-import Hero from "@/components/Global/Hero";
-import Nav from "@/components/Global/Nav";
-import Presentation from "@/components/Home/Presentation";
-import Footer from "@/components/Global/Footer";
-import Talents from "@/components/Home/Talents";
-import Collaboration from "@/components/Home/Collaboration";
-import Image from "next/image";
-import React from "react";
-import Project from "@/components/Home/Project";
-import CTA from "@/components/Global/CTA";
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+import Hero from '@/components/Global/Hero';
+import Nav from '@/components/Global/Nav';
+import Presentation from '@/components/Home/Presentation';
+import Footer from '@/components/Global/Footer';
+import Talents from '@/components/Home/Talents';
+import Collaboration from '@/components/Home/Collaboration';
+import Image from 'next/image';
+import React from 'react';
+import Project from '@/components/Home/Project';
+import CTA from '@/components/Global/CTA';
 
-const inter = Inter({subsets: ['latin']})
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-    return (<>
-        <Head>
-            <title>My Makeup</title>
-            <meta name="description" content="Votre maquilleuse directement chez vous, pour un événement, un shooting, ou une soirée,
-                    vous trouverez votre bonheur pour vous sublimer dans n'importe quelle situation !"/>
-        </Head>
-        <main className={"relative"}>
-            <Image src={"/assets/coming-soon.svg"} alt={"Coming soon"} width={"80"} height={"80"}
-                   className={"top-0 left-0 fixed z-50 m-4 p-2 bg-amber-300/75 rounded-full"}/>
-            <Nav/>
-            <Hero/>
-            <Presentation/>
-            <Talents/>
-            <Collaboration/>
-            <Project/>
-            <CTA/>
-            <Footer/>
-        </main>
-    </>)
+	return (
+		<>
+			<Head>
+				<title>My Makeup</title>
+				<meta
+					name="description"
+					content="Votre maquilleuse directement chez vous, pour un événement, un shooting, ou une soirée,
+                    vous trouverez votre bonheur pour vous sublimer dans n'importe quelle situation !"
+				/>
+			</Head>
+			<main className={'relative'}>
+				<Image
+					src={'/assets/coming-soon.svg'}
+					alt={'Coming soon'}
+					width={'80'}
+					height={'80'}
+					className={
+						'top-0 left-0 fixed z-50 m-4 p-2 bg-amber-300/75 rounded-full'
+					}
+				/>
+				<Nav />
+				<Hero />
+				<Presentation />
+				<Talents />
+				<Collaboration />
+				<Project />
+				<CTA />
+				<Footer />
+			</main>
+		</>
+	);
 }
