fae0b3d4f64aa352e1484d165938a74b67e51390
breval <breval2000@live.fr>
2023-04-28 02:04:17 +0200

    🎨 style(Nav.js, Search.js) : mise en forme du code et ajout de la fonctionnalité de recherche Le code a été mis en forme pour améliorer la lisibilité. La fonctionnalité de recherche a été ajoutée à la barre de navigation pour permettre aux utilisateurs de rechercher des maquilleuses en fonction de leur emplacement et de leur spécialité. Les champs de recherche ont été ajoutés et les valeurs saisies sont stockées dans des états. Lorsque le formulaire est soumis, les valeurs sont utilisées pour construire l'URL de recherche et rediriger l'utilisateur vers la page de résultats de recherche.

--- src/components/Global/Search.js
+++ src/components/Global/Search.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import { MapPinIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 function Search() {
+	const [searchTerm, setSearchTerm] = useState('')
+	const [city, setCity] = useState('')
+
+	const router = useRouter()
+
+	function handleSubmit(e) {
+		e.preventDefault()
+
+		if (searchTerm === '') {
+			return
+		}
+		if (city !== '') {
+			router.push(`/search?search=${searchTerm}&city=${city}`)
+		} else {
+			router.push(`/search?search=${searchTerm}`)
+		}
+	}
+
 	return (
 		<div
 			className={
 				'mb-20 flex w-full max-w-7xl justify-between rounded-2xl bg-white px-12 py-8 shadow-2xl'
 			}
 		>
-			<div className={'flex items-center gap-6'}>
-				<div className={'relative'}>
+			<form onSubmit={handleSubmit} className={' flex items-center gap-6'}>
+				<div className={' relative'}>
 					<MagnifyingGlassIcon
-						className="absolute left-4 top-1/2 h-5 w-5 -translate-y-1/2 transform text-indigo-900"
+						className=" top-1/ 2 absolute
+			left-4 h-5 w-5 -translate-y-1/2 transform text-indigo-900"
 						aria-hidden="true"
 					/>
+
 					<input
 						className={
 							'flex w-80 items-center rounded-lg border-2 border-indigo-900 bg-transparent py-2 pl-12 pr-6 text-sm leading-6 text-indigo-900'
@@ -23,6 +44,8 @@ function Search() {
 						placeholder={
 							"Essayez 'Maquilleuse mariée', 'Maquilleuse événements'..."
 						}
+						value={searchTerm}
+						onChange={e => setSearchTerm(e.target.value)}
 					/>
 				</div>
 				<div className={'relative'}>
@@ -35,18 +58,26 @@ function Search() {
 							'flex w-80 items-center rounded-lg border-2 border-indigo-900 bg-transparent py-2 pl-12 pr-6 text-sm leading-6 text-indigo-900'
 						}
 						placeholder={'Lieu de la mission (ex: Paris, Lyon, Marseille...)'}
+						value={city}
+						onChange={e => setCity(e.target.value)}
 					/>
 				</div>
-			</div>
-			<div className={'flex items-center justify-end gap-6'}>
-				<Link href="/recrutement" className={'btn-primary'}>
-					Trouver une maquilleuse
-				</Link>
-				<div className={'flex items-center justify-center'}>ou</div>
-				<Link href="/deposer-un-projet" className="">
-					<span className={'btn-primary-simple'}>Déposer un projet</span>
-				</Link>
-			</div>
+
+				<div className={'mx-8 flex items-center justify-end gap-6'}>
+					<button
+						type="submit"
+						onSubmit={handleSubmit}
+						className={'btn-primary'}
+					>
+						Trouver une maquilleuse
+					</button>
+
+					<div className={'flex items-center justify-center'}>ou</div>
+					<Link href="/deposer-un-projet" className="">
+						<span className={'btn-primary-simple'}>Déposer un projet</span>
+					</Link>
+				</div>
+			</form>
 		</div>
 	)
 }
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,202 +1,152 @@
-import Head from 'next/head';
-import React from 'react';
-import Image from 'next/image';
+import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
+import Nav from '@/components/Global/Nav'
+import Footer from '@/components/Global/Footer'
+import { useRouter } from 'next/router'
 
+function SearchPage() {
+	const [searchTerm, setSearchTerm] = useState('')
+	const [city, setCity] = useState('')
+	const [searchResults, setSearchResults] = useState([])
+	const [isSearching, setIsSearching] = useState(false)
+	const [hasSearched, setHasSearched] = useState(false)
 
-import { EnvelopeIcon, PhoneIcon } from '@heroicons/react/20/solid';
-import Nav from '@/components/Global/Nav';
-import Hero from '@/components/Global/Hero';
-import Presentation from '@/components/Home/Presentation';
-import Talents from '@/components/Home/Talents';
-import Collaboration from '@/components/Home/Collaboration';
-import Project from '@/components/Home/Project';
-import CTA from '@/components/Global/CTA';
-import Footer from '@/components/Global/Footer';
+	const router = useRouter()
 
+	useEffect(() => {
+		if (!hasSearched) {
+			const { search, city } = router.query
+			console.log(search, city)
+			if (search) {
+				setSearchTerm(search)
+			}
+			if (city) {
+				setCity(city)
+			}
 
-const people = [
+			performSearch(search, city)
+		}
+	}, [hasSearched, performSearch, router.query])
 
+	const handleSubmit = e => {
+		e.preventDefault()
+		if (searchTerm === '') {
+			return
+		}
 
-	{
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	}, {
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	}, {
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	}, {
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	}, {
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	}, {
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	}, {
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	}, {
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	},
-	{
-		name: 'Jane Cooper',
-		title: 'Paradigm Representative',
-		role: 'Admin',
-		email: 'janecooper@example.com',
-		telephone: '+1-202-555-0170',
-		imageUrl:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	},
-	// More people...
-];
+		if (city !== '') {
+			router.push(`/search?search=${searchTerm}&city=${city}`)
+			performSearch(searchTerm, city)
+		} else {
+			router.push(`/search?search=${searchTerm}`)
+			performSearch(searchTerm, city)
+		}
+	}
 
+	async function performSearch(search, city) {
+		console.log('performSearch', search, city)
 
-function Search() {
+		setIsSearching(true)
+
+		let url = `https://api.my-makeup.fr/api/searching?search=${searchTerm}&city=${city}`
+		if (city === '') {
+			url = `https://api.my-makeup.fr/api/searching?search=${searchTerm}`
+		}
+
+		const response = await fetch(url)
+		const results = await response.json()
+
+		// delay 5 seconds before setting isSearching to false
+		await new Promise(resolve => setTimeout(resolve, 2500))
+
+		setSearchResults(results)
+		setIsSearching(false)
+		setHasSearched(true)
+	}
+
 	return (
 		<>
 			<Head>
-				<title>My Makeup</title>
+				<title>My Makeup - Search</title>
 				<meta
-					name='description'
-					content='Inscription sur my-makeup.fr la plateforme qui va révolutionner votre
-                recherche de maquilleuses professionnelles, ou votre recherche de client !'
+					name="description"
+					content="Inscription sur my-makeup.fr la plateforme qui va révolutionner votre
+                recherche de maquilleuses professionnelles, ou votre recherche de client !"
 				/>
 			</Head>
 
 			<Nav />
 
-
-			<main className={'relative m-36 h-[50vh] flex justify-center items-center'}>
-
-
-				{/*	 search bar */}
-
-				<form>
-					<label htmlFor='default-search'
-								 className='mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white'>Search</label>
-					<div className='relative'>
-						<div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-							<svg aria-hidden='true' className='w-5 h-5 text-gray-500 dark:text-gray-400' fill='none'
-									 stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
-								<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2'
-											d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
-							</svg>
-						</div>
-						<input type='search' id='default-search'
-									 className='block w-full p-4 px-36 pl-10 text-sm placeholder-red-400 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-									 placeholder='Search Mockups, Logos...'
-									 name='search'
-
-									 required />
-						<button type='submit'
-										className='text-white absolute right-2.5 bottom-2.5 bg-pink-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-green-400 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Search
+			<main
+				className={'relative m-36 flex h-[50vh] items-center justify-center'}
+			>
+				<form onSubmit={handleSubmit} className="w-1/2 max-w-md">
+					<div className="mb-4">
+						<input
+							className="focus:shadow-outline w-full appearance-none rounded bg-white px-3 py-2 leading-tight text-gray-700 focus:outline-none"
+							type="text"
+							placeholder="Recherche"
+							value={searchTerm}
+							onChange={e => setSearchTerm(e.target.value)}
+						/>
+					</div>
+					<div className="flex items-center">
+						<input
+							className="focus:shadow-outline mr-2 w-full appearance-none rounded bg-white px-3 py-2 leading-tight text-gray-700 focus:outline-none"
+							type="text"
+							placeholder="Ville"
+							value={city}
+							onChange={e => setCity(e.target.value)}
+						/>
+						<button
+							className="focus:shadow-outline rounded bg-teal-500 px-4 py-2 font-bold text-white hover:bg-teal-700 focus:outline-none"
+							type="submit"
+						>
+							Valider
 						</button>
 					</div>
 				</form>
 
+				<div
+					className={
+						'mx-8 mt-8 grid w-full grid-cols-3 gap-4' +
+						(searchResults.length === 0 || isSearching ? ' hidden' : '')
+					}
+				>
+					{searchResults.map(result => (
+						// show cards
 
-				{/*	 Makeup Artist Grid */}
-
-				<ul role=' list' className=' grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-					{people.map((person) => (
-						<li
-							key={person.email}
-							className=' col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow'
+						<div
+							key={result.item.id}
+							className="flex transform cursor-pointer flex-col justify-between rounded border border-gray-200 bg-white p-4 leading-normal shadow transition duration-300 ease-in-out hover:translate-y-1 hover:scale-105 hover:border-gray-400 hover:shadow-lg"
 						>
-							<div className=' flex flex-1 flex-col p-8'>
-								<img className=' mx-auto h-32 w-32 flex-shrink-0 rounded-full' src={person.imageUrl} alt='' />
-								<h3 className=' mt-6 text-sm font-medium text-gray-900'>{person.name}</h3>
-								<dl className=' mt-1 flex flex-grow flex-col justify-between'>
-									<dt className=' sr-only'>Title</dt>
-									<dd className=' text-sm text-gray-500'>{person.title}</dd>
-									<dt className=' sr-only'>Role</dt>
-									<dd className=' mt-3'>
-			        <span
-								className=' inline-flex items-center rounded-full bg-green-50 px-2 py-1 text-xs font-medium
-							text-green-700 ring-1 ring-inset ring-green-600/20'>
-			          {person.role}
-			        </span>
-									</dd>
-								</dl>
-							</div>
-							<div>
-								<div className='-mt-px flex divide-x divide-gray-200'>
-									<div className='flex w-0 flex-1'>
-										<a
-											href={`mailto:${person.email}`}
-											className='relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900'
-										>
-											<EnvelopeIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-											Email
-										</a>
-									</div>
-									<div className='-ml-px flex w-0 flex-1'>
-										<a
-											href={`tel:${person.telephone}`}
-											className='relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900'
-										>
-											<PhoneIcon className='h-5 w-5 text-gray-400' aria-hidden='true' />
-											Call
-										</a>
-									</div>
-								</div>
-							</div>
-						</li>
+							<h3 className="mb-2 font-bold">
+								{result.item.first_name} {result.item.last_name}
+							</h3>
+							<p>Spécialité : {result.item.speciality}</p>
+							<p>Ville : {result.item.city}</p>
+							{/* ... Afficher d'autres informations si vous le souhaitez */}
+						</div>
 					))}
-				</ul>
+				</div>
 
-
+				<div
+					className={
+						'mx-8 mt-8 grid w-full grid-cols-3 gap-4' +
+						(isSearching ? ' visible' : ' hidden')
+					}
+				>
+					<div id="loader">
+						<div id="box"></div>
+						<div id="hill"></div>
+					</div>
+				</div>
 			</main>
+			{isSearching && <p>Recherche en cours...</p>}
 
 			<Footer />
-
-
 		</>
-	);
+	)
 }
 
-export default Search;
+export default SearchPage
