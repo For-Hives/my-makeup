d845473a748ebda6fe95abcc90d1215924e88266
Cinquin Andy <andy.cinquin@infotel.com>
2023-04-29 14:39:07 +0200

    enfaite le responsive c'est facile

--- src/components/Global/ResponsiveTemporary.js
+++ src/components/Global/ResponsiveTemporary.js
@@ -8,14 +8,14 @@ function ResponsiveTemporary(props) {
 				'fixed z-50 flex h-screen w-screen items-center justify-center bg-white 2xl:hidden'
 			}
 		>
-			<div className={'flex flex-col items-center justify-center gap-10'}>
+			<div className={'mx-4 flex flex-col items-center justify-center gap-10'}>
 				<Image
 					src={'/assets/phone.svg'}
 					width={100}
 					height={100}
 					alt={'return phone'}
 				/>
-				<h2>
+				<h2 className={'text-center'}>
 					Tourner votre téléphone pour une meilleure expérience, ou revenez sur
 					un écran plus grand.
 				</h2>
