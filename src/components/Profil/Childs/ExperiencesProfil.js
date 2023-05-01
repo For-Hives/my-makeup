3e829a97634cbab3a1121f19566fc8293a3c43cc
Cinquin Andy <andy.cinquin@infotel.com>
2023-05-01 17:51:34 +0200

    ♻️ refactor(ExperiencesProfil.js) : renommer le composant DescriptionProfil en ExperiencesProfil pour plus de clarté et supprimer le code inutile qui affiche la description de l'utilisateur.

--- src/components/Profil/Childs/ExperiencesProfil.js
+++ src/components/Profil/Childs/ExperiencesProfil.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export function DescriptionProfil(props) {
+export function ExperiencesProfil(props) {
 	const user = props.user
 	return (
 		<div
@@ -9,19 +9,8 @@ export function DescriptionProfil(props) {
 			}
 		>
 			<h2 className={'text-xl font-bold text-slate-700'}>
-				Vous en quelques mots
+				Expériences professionnelles
 			</h2>
-			{
-				// display the user description
-				// if \n is present, split the string and display each part in a new line
-				user?.description.split('\n').map((item, i) => {
-					return (
-						<p key={i} className={'text-slate-800'}>
-							{item}
-						</p>
-					)
-				})
-			}
 		</div>
 	)
 }
