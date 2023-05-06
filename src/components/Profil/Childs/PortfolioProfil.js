7c517615a7791dd6cadf509705f7f8d673ee94cd
Cinquin Andy <andy.cinquin@infotel.com>
2023-05-06 21:30:38 +0200

    modal new photo

--- src/components/Profil/Childs/PortfolioProfil.js
+++ src/components/Profil/Childs/PortfolioProfil.js
@@ -7,106 +7,149 @@ import { Pagination } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import Image from 'next/image'
-import { classNames } from '@/services/utils'
+import ModalUpdatePortfolioProfil from '@/components/Profil/Atoms/ModalUpdate/ModalUpdatePortfolioProfil'
+import { useRouter } from 'next/router'
 
 export function PortfolioProfil(props) {
+	// import router
+	const router = useRouter()
+	// get query param
+	const { view } = router.query
+	const mode = !!view
+
 	const user = props.user
 
+	const [modalUpdatePortfolioProfil, setModalUpdatePortfolioProfil] =
+		React.useState(false)
 	const [mySwiper, setMySwiper] = React.useState(null)
 
+	const handleModalUpdatePortfolioProfil = () => {
+		if (!mode) {
+			setModalUpdatePortfolioProfil(!modalUpdatePortfolioProfil)
+		}
+	}
+
 	return (
-		<div
-			className={
-				'flex w-full flex-col gap-4 rounded border border-slate-300 bg-white p-8'
-			}
-		>
-			<h2 className={'text-xl font-bold text-slate-700'}>Portfolio</h2>
-			<>
-				<Swiper
-					slidesPerView={'auto'}
-					spaceBetween={32}
-					pagination={{
-						clickable: true,
-					}}
-					modules={[Pagination]}
-					className="h-[500px] w-full"
-					onSlideChange={swiper => {
-						console.log(swiper.activeIndex)
-					}}
-					onInit={ev => {
-						console.log(ev)
-						setMySwiper(ev)
-					}}
-				>
-					{
-						// 	map on user?.image_gallery and return a SwiperSlide with the image
-					}
-					{user?.image_gallery?.map((image, index) => {
-						return (
-							<SwiperSlide
-								key={index}
-								style={{
-									aspectRatio: `${image.width}/${image.height}`,
-									height: '100%',
-								}}
-								className={'!h-[500px] !w-auto'}
-							>
-								<Image
-									src={image.url}
-									alt={image.alternativeText ?? image.name}
-									fill={true}
-									className={'rounded object-cover'}
-								/>
-							</SwiperSlide>
-						)
-					})}
-				</Swiper>
-			</>
-			{/* btn to go on next slide */}
-			<div className={'flex w-full items-center justify-between'}>
-				<div>
+		<div className={'w-full'}>
+			<ModalUpdatePortfolioProfil
+				modalUpdatePortfolioProfil={modalUpdatePortfolioProfil}
+				handleModalUpdatePortfolioProfil={handleModalUpdatePortfolioProfil}
+				user={user}
+			/>
+			<div
+				className={
+					(!mode ? 'group relative' : '') +
+					' flex w-full flex-col gap-4 rounded border border-slate-300 bg-white p-8'
+				}
+			>
+				{!mode ? (
 					<button
-						className={'flex items-center justify-center gap-2'}
-						onClick={() => {
-							if (mySwiper.activeIndex === 0) {
-								// 	go to the last slide
-								mySwiper.slideTo(mySwiper.slides.length - 1)
-							} else {
-								mySwiper.slidePrev()
+						onClick={handleModalUpdatePortfolioProfil}
+						className={
+							'absolute left-0 top-0 -z-10 flex h-full w-full items-center justify-center opacity-0 ' +
+							'bg-white/75 backdrop-blur-none group-hover:z-20 group-hover:opacity-100 ' +
+							'pointer-events-none transition duration-300 group-hover:pointer-events-auto group-hover:backdrop-blur-[2px] ' +
+							'user-select-none group-hover:user-select-auto focus:outline-none'
+						}
+					>
+						<div
+							className={
+								'btn-alt-primary flex items-center gap-3 bg-white text-indigo-900'
 							}
+						>
+							<span className="material-icons-round">edit</span>
+							<span className={'font-semibold'}>Modifier votre portfolio</span>
+						</div>
+					</button>
+				) : null}
+				<h2 className={'text-xl font-bold text-slate-700'}>Portfolio</h2>
+				<>
+					<Swiper
+						slidesPerView={'auto'}
+						spaceBetween={32}
+						pagination={{
+							clickable: true,
+						}}
+						modules={[Pagination]}
+						className="h-[500px] w-full"
+						onSlideChange={swiper => {
+							console.log(swiper.activeIndex)
+						}}
+						onInit={ev => {
+							console.log(ev)
+							setMySwiper(ev)
 						}}
 					>
-						<Image
-							alt={'next'}
-							src={'/assets/down-arrow.svg'}
-							className={'rotate-90'}
-							width={20}
-							height={20}
-						></Image>
-						<span className={'font-semibold text-indigo-950'}>Précédent</span>
-					</button>
-				</div>
-				<div>
-					<button
-						className={'flex items-center justify-center gap-2'}
-						onClick={() => {
-							if (mySwiper.activeIndex === mySwiper.slides.length - 1) {
-								// 	go to the first slide
-								mySwiper.slideTo(0)
-							} else {
-								mySwiper.slideNext()
-							}
-						}}
-					>
-						<span className={'font-semibold text-indigo-950'}>Suivant</span>
-						<Image
-							alt={'next'}
-							src={'/assets/down-arrow.svg'}
-							className={'-rotate-90'}
-							width={20}
-							height={20}
-						></Image>
-					</button>
+						{
+							// 	map on user?.image_gallery and return a SwiperSlide with the image
+						}
+						{user?.image_gallery?.map((image, index) => {
+							return (
+								<SwiperSlide
+									key={index}
+									style={{
+										aspectRatio: `${image.width}/${image.height}`,
+										height: '100%',
+									}}
+									className={'!h-[500px] !w-auto'}
+								>
+									<Image
+										src={image.url}
+										alt={image.alternativeText ?? image.name}
+										fill={true}
+										className={'rounded object-cover'}
+									/>
+								</SwiperSlide>
+							)
+						})}
+					</Swiper>
+				</>
+				{/* btn to go on next slide */}
+				<div className={'flex w-full items-center justify-between'}>
+					<div>
+						<button
+							className={'flex items-center justify-center gap-2'}
+							onClick={() => {
+								if (mySwiper.activeIndex === 0) {
+									// 	go to the last slide
+									mySwiper.slideTo(mySwiper.slides.length - 1)
+								} else {
+									mySwiper.slidePrev()
+								}
+							}}
+						>
+							<Image
+								alt={'next'}
+								src={'/assets/down-arrow.svg'}
+								className={'rotate-90'}
+								width={20}
+								height={20}
+							></Image>
+							<span className={'font-semibold text-indigo-950'}>Précédent</span>
+						</button>
+					</div>
+					<div>
+						<button
+							className={'flex items-center justify-center gap-2'}
+							onClick={() => {
+								if (mySwiper.activeIndex === mySwiper.slides.length - 1) {
+									// 	go to the first slide
+									mySwiper.slideTo(0)
+								} else {
+									mySwiper.slideNext()
+								}
+							}}
+						>
+							<span className={'font-semibold text-indigo-950'}>Suivant</span>
+							<Image
+								alt={'next'}
+								src={'/assets/down-arrow.svg'}
+								className={'-rotate-90'}
+								width={20}
+								height={20}
+							></Image>
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
