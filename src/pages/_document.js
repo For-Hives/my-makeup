13acda6fd7e06892f6c86290a49cb0752cd10a7c
Cinquin Andy <andy.cinquin@infotel.com>
2023-05-14 23:39:12 +0200

    🔥 supprimer(ResumeProfil.js) : supprimer les commentaires inutiles et les imports inutilisés 🔥 supprimer(_document.js) : supprimer le lien vers la police de caractères inutilisée

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,10 +6,6 @@ export default function Document() {
 		<Html lang="fr">
 			<Head>
 				<link rel="icon" href="/favicon.webp" />
-				{/*<link*/}
-				{/*	rel="stylesheet"*/}
-				{/*	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"*/}
-				{/*/>*/}
 				<Script
 					strategy={'afterInteractive'}
 					src="https://www.googletagmanager.com/gtag/js?id=G-7M05RC6YDH"
