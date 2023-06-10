ca9d63f2ad6181dfa6253787a0e17a9c9ea06b9b
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-10 13:53:33 +0200

    blog article possibility

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,25 +1,46 @@
 import { getArticleIdList, getArticlesDetails } from '../../../lib/articles'
 import Head from 'next/head'
-import Nav from '@components/Global/Nav'
+import Nav from '@/components/Global/Nav'
 import Image from 'next/image'
-import Router from 'next/router'
+import Router, { useRouter } from 'next/router'
 import React from 'react'
-import Footer from '@components/Global/Footer'
+import Footer from '@/components/Global/Footer'
 import ResponsiveTemporary from '@/components/Global/ResponsiveTemporary'
-import Hero from '@/components/Global/Hero'
-import Link from 'next/link'
 import CTA from '@/components/Global/CTA'
-import { useQuery } from '@tanstack/react-query'
+import { convertToStringDate } from '@/services/utils'
+import Link from 'next/link'
+
+function ArrowLeftIcon(props) {
+	return (
+		<svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
+			<path
+				d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5"
+				strokeWidth="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	)
+}
 
 export default function Article({ articleData }) {
+	let router = useRouter()
+
+	console.log('----------- log ------------')
+	console.log('articleData', articleData)
+
+	const meta = articleData
+
 	return (
 		<>
 			<Head>
-				<title>My Makeup</title>
+				<title>{meta?.seo_title ?? 'My Makeup'}</title>
 				<meta
 					name="description"
-					content="L'actualité de My-Makeup, ce que nous faisons pour améliorer votre quotidien !
-					Et les nouveautés qui arrivent bientôt !"
+					content={
+						meta?.seo_description ??
+						'Découvrez cet article passionnant de la part de My Makeup'
+					}
 				/>
 			</Head>
 			<div className={'relative'}>
@@ -35,59 +56,37 @@ export default function Article({ articleData }) {
 				<Nav />
 				<main className={'relative'}>
 					<ResponsiveTemporary />
-					<section className={'relative py-20'}>
-						<div className="mx-auto max-w-7xl">
-							<div className="mx-auto mb-10">
-								<h2 className="text-start text-4xl font-bold tracking-tight text-slate-900 sm:text-4xl">
-									Gardez votre projet beauté en tête, on s&apos;occupe du reste
-								</h2>
-								<p className="mt-6 w-1/2 text-start text-lg text-slate-700">
-									Trouvez les meilleures maquilleuses, planifiez votre projet,
-									payez et recevez des paiements, le tout dans une solution
-									unifiée. Oui, vous avez bien lu.
-								</p>
-							</div>
-
-							<section className={'mx-auto flex max-w-7xl gap-32'}>
-								<div className={'w-1/2'}>
-									<div className={'flex flex-col gap-2'}>
-										{articles &&
-											articles.map(article => (
-												<div key={article.id}>
-													{/*<p>{article.attributes.updatedAt.date}</p>*/}
-													<h2
-														className={'mb-4 text-xl font-bold text-slate-700'}
-													>
-														{article.attributes.title}
-													</h2>
-													{/*<div*/}
-													{/*	className={'text-sm text-slate-500'}*/}
-													{/*	dangerouslySetInnerHTML={article.attributes.content}*/}
-													{/*/>*/}
-													<Link
-														className={''}
-														href={`/blog/${article.attributes.slug}`}
-													>
-														Read article {'>'}
-													</Link>
-												</div>
-											))}
-									</div>
-								</div>
-								<div className={'flex w-1/2 items-center justify-center'}>
-									<Image
-										className={
-											'h-[500px] w-full rounded-2xl object-cover object-top'
-										}
-										src={'/assets/maquilleuse_project.webp'}
-										alt={'illustration'}
-										width={'500'}
-										height={'350'}
-									/>
-								</div>
-							</section>
-						</div>
-					</section>
+					<div className="mx-auto max-w-2xl">
+						{
+							<Link
+								type="button"
+								href={'/blog'}
+								aria-label="Go back to articles"
+								className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0"
+							>
+								<ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+							</Link>
+						}
+						{meta && (
+							<article>
+								<header className="flex flex-col">
+									<h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+										{meta.title}
+									</h1>
+									<time
+										dateTime={meta.updatedAt}
+										className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+									>
+										<span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+										<span className="ml-3">
+											{convertToStringDate(meta.updatedAt)}
+										</span>
+									</time>
+								</header>
+								{/*<div className="prose mt-8 xl:prose-xl">{meta.content}</div>*/}
+							</article>
+						)}
+					</div>
 
 					<CTA />
 				</main>
@@ -105,15 +104,16 @@ export async function getStaticPaths() {
 			'Content-Type': 'application/json',
 			Accept: 'application/json',
 		},
-	})
+	}).then(res => res.json())
 
 	/**
 	 * format the data for getStaticPaths
 	 * @type {{params: {id: *}}[]}
 	 */
-	const paths = res.map(record => ({
+	const paths = res?.data?.map(record => ({
 		params: {
-			id: record.id,
+			id: record.attributes.slug,
+			data: record.id,
 		},
 	}))
 	return {
@@ -124,7 +124,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
 	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_API_URL}api/article/${params.id}`,
+		`${process.env.NEXT_PUBLIC_API_URL}api/article/${params.data}`,
 		{
 			method: 'GET',
 			headers: {
@@ -134,17 +134,24 @@ export async function getStaticProps({ params }) {
 			},
 		}
 	)
+	console.log('----------- log ------------')
+	console.log('params', params)
 
-	const albumData = await getArticlesDetails(params.id)
+	console.log('----------- log ------------')
+	console.log(
+		'\t\t`${process.env.NEXT_PUBLIC_API_URL}api/article/${params.id}`,\n',
+		`${process.env.NEXT_PUBLIC_API_URL}api/article/${params.id}`
+	)
+	const articleData = await getArticlesDetails(params.id)
 
-	if (!albumData) {
+	if (!articleData) {
 		return {
 			props: { hasError: true },
 		}
 	}
 	return {
 		props: {
-			albumData,
+			articleData,
 		},
 	}
 }
