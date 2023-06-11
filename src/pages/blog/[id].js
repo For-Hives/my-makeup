b7767b728ba569ac0843eae4ee1e421bbcd9c248
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-11 14:24:51 +0200

    article call api functionnal

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -63,33 +63,37 @@ export default function Article({ articleData }) {
 									type="button"
 									href={'/blog'}
 									aria-label="Go back to articles"
-									className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0"
+									className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-gray-800/5 ring-1 ring-gray-900/5 transition dark:border dark:border-gray-700/50 dark:bg-gray-800 dark:ring-0 dark:ring-white/10 dark:hover:border-gray-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0"
 								>
-									<ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+									<ArrowLeftIcon className="h-4 w-4 stroke-gray-500 transition group-hover:stroke-gray-700 dark:stroke-gray-500 dark:group-hover:stroke-gray-400" />
 								</Link>
 							}
 							{meta && (
 								<article>
 									<header className="flex flex-col">
-										<h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+										<h1 className="mt-6 text-3xl font-bold tracking-tight text-gray-800 dark:text-gray-100 sm:text-4xl">
 											{meta.title}
 										</h1>
 										<time
 											dateTime={meta.updatedAt.toString()}
-											className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+											className="order-first flex items-center text-base text-gray-400 dark:text-gray-500"
 										>
-											<span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+											<span className="h-4 w-0.5 rounded-full bg-gray-200 dark:bg-gray-500" />
 											<span className="ml-3">
 												{convertToStringDate(meta.updatedAt)}
 											</span>
 										</time>
 									</header>
 									<div
-										className="prose mt-8 xl:prose-xl"
+										className="prose my-8 xl:prose-lg"
 										dangerouslySetInnerHTML={{
 											__html: meta.content.toString(),
 										}}
 									/>
+									<h3 className={'flex items-center text-base text-gray-400'}>
+										<span className="h-4 w-0.5 rounded-full bg-gray-200 dark:bg-gray-500" />
+										<span className="ml-3">Auteur.e : {meta.author}</span>
+									</h3>
 								</article>
 							)}
 						</div>
