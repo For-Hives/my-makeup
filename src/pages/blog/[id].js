d0d90f3df3cedd198dfa287e68ea831cc8fc135c
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-11 13:57:46 +0200

    blog generation ssg done

--- src/pages/blog/[id].js
+++ src/pages/blog/[id].js
@@ -1,8 +1,7 @@
-import { getArticleIdList, getArticlesDetails } from '../../../lib/articles'
 import Head from 'next/head'
 import Nav from '@/components/Global/Nav'
 import Image from 'next/image'
-import Router, { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 import React from 'react'
 import Footer from '@/components/Global/Footer'
 import ResponsiveTemporary from '@/components/Global/ResponsiveTemporary'
@@ -26,7 +25,9 @@ function ArrowLeftIcon(props) {
 export default function Article({ articleData }) {
 	let router = useRouter()
 
-	const meta = articleData
+	const meta = articleData.attributes
+
+	console.log(articleData)
 
 	return (
 		<>
@@ -53,38 +54,39 @@ export default function Article({ articleData }) {
 				<Nav />
 				<main className={'relative'}>
 					<ResponsiveTemporary />
-					<div className="mx-auto max-w-2xl">
-						{
-							<Link
-								type="button"
-								href={'/blog'}
-								aria-label="Go back to articles"
-								className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0"
-							>
-								<ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
-							</Link>
-						}
-						{meta && (
-							<article>
-								<header className="flex flex-col">
-									<h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-										{meta.title}
-									</h1>
-									<time
-										dateTime={meta.updatedAt}
-										className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
-									>
-										<span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
-										<span className="ml-3">
-											{convertToStringDate(meta.updatedAt)}
-										</span>
-									</time>
-								</header>
-								{/*<div className="prose mt-8 xl:prose-xl">{meta.content}</div>*/}
-							</article>
-						)}
+					<div className={'relative mx-auto my-48 max-w-7xl'}>
+						<div className="mx-auto max-w-2xl">
+							{
+								<Link
+									type="button"
+									href={'/blog'}
+									aria-label="Go back to articles"
+									className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 transition dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20 lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0"
+								>
+									<ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+								</Link>
+							}
+							{meta && (
+								<article>
+									<header className="flex flex-col">
+										<h1 className="mt-6 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+											{meta.title}
+										</h1>
+										<time
+											dateTime={meta.updatedAt}
+											className="order-first flex items-center text-base text-zinc-400 dark:text-zinc-500"
+										>
+											<span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500" />
+											<span className="ml-3">
+												{convertToStringDate(meta.updatedAt)}
+											</span>
+										</time>
+									</header>
+									{/*<div className="prose mt-8 xl:prose-xl">{meta.content}</div>*/}
+								</article>
+							)}
+						</div>
 					</div>
-
 					<CTA />
 				</main>
 				<Footer />
@@ -118,9 +120,9 @@ export async function getStaticPaths() {
 	}
 }
 
-export async function getStaticProps({ params, data }) {
-	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_API_URL}api/article/${params.data}`,
+export async function getStaticProps({ params }) {
+	let articleData = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}api/articles?filters[slug][$eq]=${params.id}`,
 		{
 			method: 'GET',
 			headers: {
@@ -129,16 +131,9 @@ export async function getStaticProps({ params, data }) {
 				Accept: 'application/json',
 			},
 		}
-	)
-	console.log('----------- log ------------')
-	console.log('params', params)
+	).then(res => res.json())
 
-	console.log('----------- log ------------')
-	console.log(
-		'\t\t`${process.env.NEXT_PUBLIC_API_URL}api/article/${params.id}`,\n',
-		`${process.env.NEXT_PUBLIC_API_URL}api/article/${params.id}`
-	)
-	const articleData = await getArticlesDetails(params.id)
+	articleData = articleData?.data?.[0]
 
 	if (!articleData) {
 		return {
