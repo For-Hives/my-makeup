6d04171100965b32e54199eb79ef6deeab0e38e1
CinquinAndy <cinquin.andy@gmail.com>
2023-06-12 13:51:47 +0200

    🎨 style(package.json) : mettre à jour la version de la bibliothèque mailgun.js ✨ fonctionnalité(sendMail.js) : utiliser la bibliothèque form-data pour créer un client mailgun et envoyer un email

--- src/pages/api/sendMail.js
+++ src/pages/api/sendMail.js
@@ -1,34 +1,21 @@
-import mailgun from 'mailgun-js'
-
-const mg = mailgun.client({
-	apiKey: process.env.MAILGUN_API_KEY,
-	domain: process.env.MAILGUN_DOMAIN,
-})
+const formData = require('form-data')
+const Mailgun = require('mailgun.js')
+const mailgun = new Mailgun(formData)
+const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY })
 
 export default function handler(req, res) {
 	console.log(req.body)
 	if (req.method === 'POST') {
-		const data = {
-			from: 'My Makeup <contact@my-makeup.fr>',
-			to: 'cinquin.andy@gmail.com',
-			subject: 'Nouveau message de contact',
-			text: `
-                Nom: ${req.body.lastName} \n
-                Prénom: ${req.body.firstName} \n
-                Entreprise: ${req.body.company} \n
-                Email: ${req.body.email} \n
-                Numéro de téléphone: ${req.body.phoneNumber} \n
-                Message: ${req.body.message}
-            `,
-		}
-
-		mg.messages().send(data, function (error, body) {
-			if (error) {
-				res.status(500).json({ success: false })
-			} else {
-				res.status(200).json({ success: true })
-			}
-		})
+		mg.messages
+			.create(process.env.MAILGUN_DOMAIN, {
+				from: 'Excited User <mailgun@sandbox-123.mailgun.org>',
+				to: ['cinquin.andy@gmail.com'],
+				subject: 'Test - Hello',
+				text: 'Testing some Mailgun awesomeness!',
+				html: '<h1>Testing some Mailgun awesomeness!</h1>',
+			})
+			.then(msg => console.log(msg)) // logs response data
+			.catch(err => console.log(err)) // logs any error
 	} else {
 		res.status(405).json({ message: 'Method not allowed' })
 	}
