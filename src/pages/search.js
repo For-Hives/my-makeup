e502e91539905ba7611f200ea8e5bca9e85482db
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-13 11:57:21 +0200

    🐈 completez la recherche ~ design

--- src/pages/search.js
+++ src/pages/search.js
@@ -106,6 +106,23 @@ function SearchPage() {
 				<main className={'relative'}>
 					<ResponsiveTemporary />
 					<FullSearchBloc />
+					{!hasSearched && !isSearching && (
+						<div className={'flex w-full items-center justify-center py-32'}>
+							<div
+								className={'flex flex-col items-center justify-center gap-8'}
+							>
+								<Image
+									src={'/assets/vectorials-used/catSearch.svg'}
+									alt={'search'}
+									width={300}
+									height={300}
+								/>
+								<h2 className={'text-3xl text-gray-700'}>
+									Complétez la recherche pour voir des résultats
+								</h2>
+							</div>
+						</div>
+					)}
 				</main>
 				<Footer />
 			</div>
