5749bc1b607ae1cced362479598eeb4d2562bcad
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-14 11:35:06 +0200

    🔥 supprimer(Card-demo.js) : retirer l'importation de FireIcon qui n'est plus utilisé 🔥 supprimer(loader.css) : retirer le code CSS inutilisé pour la pagination swiper ✨ fonctionnalité(loader.css) : ajouter une couleur personnalisée pour l'icône SVG de thé (tea)

--- src/components/Global/Card-demo.js
+++ src/components/Global/Card-demo.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import { FireIcon } from '@heroicons/react/24/outline'
 import { StarIcon, HeartIcon } from '@heroicons/react/20/solid'
 import { BadgeSuperMaquilleuse } from '@/components/Global/BadgeSuperMaquilleuse'
 
