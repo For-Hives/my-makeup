f7c96294b1472ad7d90c426a0a021fcecc43013b
Cinquin Andy <andy.cinquin@infotel.com>
2023-06-18 22:58:59 +0200

    🎨 style(signin.js) : supprimer la classe CSS inutile et remplacer le texte du bouton "Retourner à mon profil" par "Retourner sur mon profil"

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -257,12 +257,11 @@ function Signin() {
 								</h2>
 
 								<Link
+									type="submit"
+									className="btn-alt-primary mt-8"
 									href={'/auth/profil'}
-									className={
-										'radius-2xl my-8 border-2 border-indigo-700 px-4 py-2 text-indigo-700 hover:text-indigo-700 hover:underline'
-									}
 								>
-									Retourner à mon profile
+									Retourner sur mon profil
 								</Link>
 
 								<button
