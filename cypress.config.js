b8ddaba0afa1f316657b2ce5916994b3e6210b4a
Fade <math44100@yahoo.fr>
2023-06-25 14:25:20 +0200

    modif configuration cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
 	e2e: {
 		baseUrl: 'http://localhost:3000',
 		defaultCommandTimeout: 10000,
+		numTestsKeptInMemory: 50, // Par défaut 50, mais si un jour les tests crash pour "out of memory" ne pas hésiter à baisser cette valeur.
+		video: false,
 		reporter: 'junit',
 		reporterOptions: {
 			reporterEnabled: 'mochawesome',
