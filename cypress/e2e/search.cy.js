efb139c207f22507a6d25c37680a0fd24faf402d
brlef <breval.lefloch@rca.fr>
2023-06-27 17:08:46 +0200

    🔧 chore(.gitignore): add cypress/screenshots/ directory to .gitignore 🔧 chore(cypress.config.js): disable chrome web security 🔧 chore(package.json): add cypress-social-logins and @testing-library/cypress as dev dependencies 🔧 chore(package.json): add cypress:open script 🔧 chore(_app.js): reformat code ✨ feat(signin.cy.js): add e2e test for Google social login ✨ feat(search.cy.js): add e2e tests for search page

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,5 +1,14 @@
-describe('test', () => {
-	it('tests test', () => {
+describe('test de la page de recherche', () => {
+	it('Recherche par terme uniquement', () => {
+		cy.visit('http://localhost:3000/search')
+		cy.get("[data-cy='search-input']").click()
+		cy.get("[data-cy='search-input']").type('Test')
+		cy.get("[data-cy='search-button']").click()
+		// at least one result
+		cy.get("[data-cy='search-result']").first().should('exist')
+	})
+
+	it('Recherche par terme et par ville', () => {
 		cy.visit('http://localhost:3000/search')
 		cy.get("[data-cy='search-input']").click()
 		cy.get("[data-cy='search-input']").type('Test')
