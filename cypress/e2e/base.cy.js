136c35b8d13763ad5a18a04aabc9c744ec926ea9
breval <breval2000@live.fr>
2023-06-28 01:27:08 +0200

    🚀 fonctionnalité(signin.cy.js) : ajouter des logs pour améliorer la lisibilité des tests Ajout de logs pour améliorer la lisibilité des tests et faciliter la compréhension de ce qui se passe lors de l'exécution des tests.
    🚀 fonctionnalité(base.cy.js) : ajouter un test pour vérifier le format du sitemap
    Ajout d'un test pour vérifier le format du sitemap. Le test utilise la bibliothèque x2js pour convertir le sitemap XML en JSON et vérifier que chaque URL est valide.
    
    📦 dépendance(package.json) : ajouter la dépendance x2js
    Ajout de la dépendance x2js pour permettre la conversion du sitemap XML en JSON dans le test ajouté.

--- cypress/e2e/base.cy.js
+++ cypress/e2e/base.cy.js
@@ -1,5 +1,34 @@
+const X2JS = require('x2js')
 describe('Test des pages', () => {
 	it('page par defauts - verifie si cypress marche', () => {
 		cy.visit('http://localhost:3000/')
+	})
+
+	it('verifie si le sitemap a le bon format', () => {
+		cy.request('/sitemap.xml')
+			.its('body')
+			.then(body => {
+				const x2js = new X2JS()
+				const json = x2js.xml2js(body)
+				// get all URLs from the sitemap
+				expect(json.urlset.url).to.be.an('array').and.have.length.gt(0)
+
+				// get all URLs from the sitemap
+				expect(json.urlset.url).to.be.an('array').and.have.length.gt(0)
+
+				// todo : check if each URL is valid
+
+				// json.urlset.url.forEach((url) => {
+				// 	const parsed = new URL('http://localhost:3000' + url.loc);
+				// 	cy.log(parsed.pathname);
+				//
+				// 	// check if the resource exists
+				// 	cy.request('HEAD', url.loc).its('status').should('eq', 200);
+				// 	// check if the resource exists AND download it
+				// 	cy.request(url.loc).its('status').should('eq', 200);
+				// 	// visit the page to check if it loads in the browser
+				// 	cy.visit(url.loc).wait(1000, { log: false });
+				// });
+			})
 	})
 })
