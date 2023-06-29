46a67613e4ba49a022b7920227a1244557d15827
brlef <breval.lefloch@rca.fr>
2023-06-29 16:53:37 +0200

    🐛 fix(signin.cy.js): fix indentation and formatting issues ✨ feat(signin.cy.js): add tests for login with Google, Facebook, and Email/Password

--- cypress/e2e/auth/signin.cy.js
+++ cypress/e2e/auth/signin.cy.js
@@ -5,8 +5,45 @@ describe('Login page', () => {
 	})
 
 	it('Login with Google', () => {
-		const username = Cypress.env('GOOGLE_USER')
-		const password = Cypress.env('GOOGLE_PW')
+		cy.get("[data-cy='google-signin']").should('exist')
+
+		cy.intercept('GET', '/api/auth/providers').as('getProviders')
+		cy.intercept('POST', '/api/auth/signin/google?').as('getGoogle')
+		cy.get("[data-cy='google-signin']")
+			.click()
+			.then(() => {
+				cy.wait('@getProviders').its('response.statusCode').should('eq', 304)
+				cy.wait('@getGoogle').its('response.statusCode').should('eq', 200)
+			})
+	})
+
+	it('Login with Facebook', () => {
+		cy.get("[data-cy='facebook-signin']").should('exist')
+
+		cy.intercept('GET', '/api/auth/providers').as('getProviders')
+		cy.intercept('POST', '/api/auth/signin/facebook?').as('getFacebook')
+		cy.get("[data-cy='facebook-signin']")
+			.click()
+			.then(() => {
+				cy.wait('@getProviders').its('response.statusCode').should('eq', 304)
+				cy.wait('@getFacebook').its('response.statusCode').should('eq', 200)
+			})
+	})
+
+	it('Login with Email / Password', () => {
+		cy.get("[data-cy='email-input']").click()
+		cy.get("[data-cy='email-input']").type('breval2000@live.fr')
+		cy.get("[data-cy='password-input']").type('@linuxAP5')
+		cy.type('{enter}')
+
+		// check if facebook login button exists and if it does, if it opens a popup
+		cy.get(`a[href="${Cypress.env('SITE_NAME')}/api/auth/signin/facebook"]`)
+			.should('exist')
+			.and('have.attr', 'target', '_blank')
+			.and('have.attr', 'rel', 'noopener noreferrer')
+
+		const username = Cypress.env('FACEBOOK_USER')
+		const password = Cypress.env('FACEBOOK_PW')
 		const loginUrl = Cypress.env('SITE_NAME')
 		const cookieName = Cypress.env('COOKIE_NAME')
 
@@ -21,12 +58,12 @@ describe('Login page', () => {
 			isPopup: true,
 			loginSelector: `a[href="${Cypress.env(
 				'SITE_NAME'
-			)}/api/auth/signin/google"]`,
+			)}/api/auth/signin/facebook"]`,
 			postLoginSelector: '.unread-count',
 		}
 
 		return cy
-			.task('GoogleSocialLogin', socialLoginOptions)
+			.task('FacebookSocialLogin', socialLoginOptions)
 			.then(({ cookies }) => {
 				cy.clearCookies()
 
