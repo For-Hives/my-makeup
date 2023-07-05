d9ac74a4e1b3bfb385c990e88b47bc169b0a57c1
breval <breval2000@live.fr>
2023-07-05 17:58:52 +0200

    🔧 corriger(profil.cy.js, signin.cy.js) : utiliser les variables d'environnement pour les informations de connexion Les adresses e-mail et les mots de passe utilisés pour la connexion sont maintenant récupérés à partir des variables d'environnement TEST_USER et TEST_PW respectivement. Cela permet de garder les informations de connexion confidentielles et de les gérer plus facilement en utilisant des variables d'environnement.

--- cypress/e2e/auth/signin.cy.js
+++ cypress/e2e/auth/signin.cy.js
@@ -3,8 +3,8 @@ it('Login with Email / Password', () => {
 
 	cy.visit('http://localhost:3000/auth/signin')
 	cy.get("[data-cy='email-input']").click()
-	cy.get("[data-cy='email-input']").type('test@forhive.fr')
-	cy.get("[data-cy='password-input']").type('TESTtest@1')
+	cy.get("[data-cy='email-input']").type(Cypress.env('TEST_USER'))
+	cy.get("[data-cy='password-input']").type(Cypress.env('TEST_PW'))
 	cy.get("[data-cy='email-signin']").click()
 
 	cy.wait('@getCredentials').its('response.statusCode').should('eq', 200)
