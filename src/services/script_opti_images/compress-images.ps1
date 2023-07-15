909aba86b99459fb45ae0eec3d6e2988bacaac61
Cinquin Andy <andy.cinquin@infotel.com>
2023-07-15 19:07:51 +0200

    news illustrations, dev assets

--- src/services/script_opti_images/compress-images.ps1
+++ src/services/script_opti_images/compress-images.ps1
@@ -1,8 +1,9 @@
-# Chemin du dossier contenant les images à convertir
-$cheminDossierImages = "C:\Users\andyq\DocumentsAndy\ProjetPro2021_2022\my-makeup\public\assets\cats\10295552-family"
+# Chemin du dossier contenant les images ï¿½ convertir
+$cheminDossierImages = "C:\Users\andyq\DocumentsAndy\ProjetPro2022_2023\my-makeup\public\assets\illustrations"
 
-# Construction de la commande pour convertir l'image en WebP avec 75% de qualité
-$commande = "C:\Users\andyq\libwebp-1.3.0-windows-x64\bin\cwebp.exe -q 75"
+# Construction de la commande pour convertir l'image en WebP avec 75% de qualitï¿½
+# $commande = "C:\Users\andyq\libwebp-1.3.0-windows-x64\bin\cwebp.exe -q 75"
+$commande = "C:\Users\andyq\Documents\libwebp-1.3.0-windows-x64\bin\cwebp.exe -q 75"
 
 # Obtention de la liste des fichiers d'images dans le dossier
 $images = Get-ChildItem -Path $cheminDossierImages -File | Where-Object {
@@ -17,6 +18,6 @@ foreach ($image in $images) {
     # Chemin du fichier de destination en WebP
     $cheminFichierDestination = [System.IO.Path]::ChangeExtension($cheminFichierSource, "webp")
 
-    # Exécution de la commande
+    # Exï¿½cution de la commande
     $process = Invoke-Expression "$commande `"$cheminFichierSource`" -o `"$cheminFichierDestination`""
 }
