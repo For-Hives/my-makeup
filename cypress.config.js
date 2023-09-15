acf4ca1435ee6c9491a60a6d842e700589faacf4
breval <breval2000@live.fr>
2023-09-15 23:54:18 +0200

    Update Cypress configuration with retries and comments
    - Add retries configuration for run mode and open mode
    - Increase default command timeout to 30000 milliseconds
    - Keep 50 tests in memory
    - Enable video recording during tests
    - Use webpack bundler for e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
 	e2e: {
 		projectId: 'xvg25a',
 		baseUrl: 'http://localhost:3000',
+		retries: {
+			runMode: 3,
+			openMode: 0,
+		},
 		defaultCommandTimeout: 30000,
 		numTestsKeptInMemory: 50, // Par défaut 50, mais si un jour les tests crash pour "out of memory" ne pas hésiter à baisser cette valeur.
 		video: true,
