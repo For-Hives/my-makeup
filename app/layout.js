86684d3a949d6e13083a4e71a4e1c7c446093684
fansoa <fverin.contactpro@gmail.com>
2024-08-28 17:39:02 +0200

    create RootLayout for App Router

---
+++ app/layout.js
@@ -0,0 +1,46 @@
+import Script from 'next/script'
+
+import 'material-icons/iconfont/material-icons.css'
+import '@/styles/globals.css'
+import '@/styles/stars.css'
+import '@/styles/loader.css'
+import '@/styles/blog.css'
+import '@/styles/burger_menu.css'
+import 'react-toastify/dist/ReactToastify.css'
+
+import { ToastContainer } from 'react-toastify'
+import SessionProviderWrapper from 'providers/SessionProviderWrapper'
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="fr">
+			<Script
+				async
+				src="https://umami.wadefade.fr/script.js"
+				strategy={'afterInteractive'}
+				data-domains={'my-makeup.fr'}
+				data-website-id="e7010ee5-a940-4add-80bf-5483d2c515db"
+			/>
+			<Script
+				strategy={'afterInteractive'}
+				src="https://www.googletagmanager.com/gtag/js?id=G-6Z5K42B5YH"
+			/>
+			<Script id={'analytics'} strategy="afterInteractive">
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-6Z5K42B5YH');
+        `}
+			</Script>
+			<body className={'bg-neutral-50'}>
+				<main>
+					<SessionProviderWrapper>
+						{children}
+						<ToastContainer />
+					</SessionProviderWrapper>
+				</main>
+			</body>
+		</html>
+	)
+}
