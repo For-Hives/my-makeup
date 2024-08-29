04c0b8045d1e774fe3e6c8e807f8ab2e9c204a75
fansoa <fverin.contactpro@gmail.com>
2024-08-29 19:10:41 +0200

    migrate pour-les-maquilleuses

--- app/solutions/pour-les-maquilleuses/page.js
+++ app/solutions/pour-les-maquilleuses/page.js
@@ -1,32 +1,21 @@
-import React from 'react'
-
-import Head from 'next/head'
-
 import Footer from '@/components/Global/Footer'
 import Hero from '@/components/Global/Hero'
 import Nav from '@/components/Global/Nav'
 import CTA from '@/components/Global/CTA'
 
-/**
- * @param props
- * @constructor
- */
-function PourLesMaquilleuses(props) {
+export const metadata = {
+	description:
+		"Avec notre plateforme, donner un coup de boost à votre carrière n'a jamais été aussi simple !",
+	// seo tag canonical link
+	alternates: {
+		canonical: 'https://my-makeup.fr/solutions/pour-les-maquilleuses',
+	},
+	title: 'Solutions My-Makeup pour les Maquilleuses !',
+}
+
+function PourLesMaquilleuses() {
 	return (
 		<>
-			<Head>
-				<title>Solutions My-Makeup pour les Maquilleuses !</title>
-				<meta
-					content="Avec notre plateforme, donner un coup de boost à votre carrière n'a jamais été aussi simple !"
-					name="description"
-				/>
-				{/*	seo tag canonical link */}
-				<link
-					href="https://my-makeup.fr/solutions/pour-les-maquilleuses"
-					rel="canonical"
-				/>
-			</Head>
-
 			<Nav />
 
 			<main className={'relative'}>
