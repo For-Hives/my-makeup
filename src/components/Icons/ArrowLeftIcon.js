997030bc30f02b2280278e49405ca32754f081e3
fansoa <fverin.contactpro@gmail.com>
2024-08-30 22:03:17 +0200

    add ArrowLeftIcon to Components Icons

---
+++ src/components/Icons/ArrowLeftIcon.js
@@ -0,0 +1,14 @@
+function ArrowLeftIcon(props) {
+	return (
+		<svg aria-hidden="true" fill="none" viewBox="0 0 16 16" {...props}>
+			<path
+				d="M7.25 11.25 3.75 8m0 0 3.5-3.25M3.75 8h8.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="1.5"
+			/>
+		</svg>
+	)
+}
+
+export default ArrowLeftIcon
