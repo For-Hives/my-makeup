18be618e1d342eef5c2d29e560b091d95a683ffa
Cinquin Andy <andy.cinquin@infotel.com>
2024-08-31 14:40:55 +0200

    Fix css issues
    Added a new content path for additional files in the project. This change expands the scope of Tailwind CSS to include files from a different directory.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
 			colors: {
@@ -11,4 +10,5 @@ module.exports = {
 		},
 	},
 	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+	content: ['./src/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}'],
 }
